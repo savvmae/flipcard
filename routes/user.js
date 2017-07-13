73bf75d348fce422d01fcd6b1ed9e6acaf963de4
Savannah Cosby <savannah.cosby123@gmail.com>
2017-07-13 10:00:59 -0400

    fixed minor bugs

--- routes/user.js
+++ routes/user.js
@@ -24,19 +24,33 @@ router.post('/api/signup', (request, response) => {
         var model = {
             message: "That email is already registered. Log in to continue."
         }
-        return response.render('login', model );
+        return response.render('login', model);
     }
     else if (request.body.email && request.body.password) {
         var hashed = crypto.pbkdf2Sync(request.body.password, 'salt', 10, 512, 'sha512').toString('base64');
+
         var user = {
             email: request.body.email,
             password: hashed,
         }
         data.users.push(user);
         request.session.isAuthenticated = true;
+        var model = {
+
+        }
         var itemsJSON = JSON.stringify(data);
         fs.writeFileSync('data.json', itemsJSON);
-        return response.render('dashboard');
+        if (data.decks[0] != undefined) {
+            var modelStatus = {
+                message: "success",
+                data: data.decks
+            }
+            // return response.status(200).json(modelStatus);
+            return response.render('dashboard', modelStatus);
+
+        } else {
+            return response.status(400).json({ message: "Route Failed" });
+        }
         // return response.status(200).json({ email: user.email, message: "Success" });
 
     } else {
@@ -48,13 +62,13 @@ router.post('/api/login', (request, response) => {
 
     var hashed = crypto.pbkdf2Sync(request.body.password, 'salt', 10, 512, 'sha512').toString('base64');
     var userEmail = data.users.find(user => { return user.email === request.body.email });
-    var userPW = data.users.find(user => { return  user.password === hashed });
+    var userPW = data.users.find(user => { return user.password === hashed });
 
     if (userEmail && userPW) {
         request.session.isAuthenticated = true;
         return response.redirect('/api/decks');
         // return response.status(200).json({ message: "Success", data: userEmail.email});
-    } else if(userEmail){
+    } else if (userEmail) {
         return response.status(400).json({ message: "Incorrect password" });
     }
     else {
